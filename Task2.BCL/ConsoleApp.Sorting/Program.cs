b42f734ba993e0f409482d968d0315667ee71bba
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2017-04-14 23:56:35 +0300

    Add Task 2 - BCL 2

---
+++ Task2.BCL/ConsoleApp.Sorting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Sorting;
+
+namespace ConsoleApp.Sorting
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var array = new [] {"A", "9", "d", "Ab", "ä", "a", "aB", "m", "aa", "ab", "ss", "ß", "Ä", "0", "Äb", "äb", "Z", "z" };
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Initial");
+            Console.WriteLine(string.Join(", ", array));
+
+            ArraySorting.Sort(array);
+            Console.WriteLine("Ascending");
+            Console.WriteLine(string.Join(", ", array));
+
+            ArraySorting.Sort(array, false);
+            Console.WriteLine("Descending");
+            Console.WriteLine(string.Join(", ", array));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2.BCL/Sorting/ArraySorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sorting
+{
+    public static class ArraySorting
+    {
+        public static void Sort(string[] array, bool ascSortOrder = true)
+        {
+            var comparer = StringComparer.InvariantCulture;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[i], array[j]) > 0 == ascSortOrder)
+                    {
+                        Swap(array, i, j);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(string[] array, int firstIndex, int secondIndex)
+        {
+            string temp = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
+    }
+}
