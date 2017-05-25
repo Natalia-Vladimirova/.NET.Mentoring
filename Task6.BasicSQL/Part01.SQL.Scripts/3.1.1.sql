27e83ce5be19414148913f537fb4ec333d1ea1d2
Natalia Vladimirova <Natalia_Vladimirova@epam.com>
2017-05-25 13:58:53 +0300

    Task 6 part 1 - add scripts for 3.1 task

---
+++ Task6.BasicSQL/Part01.SQL.Scripts/3.1.1.sql
@@ -0,0 +1,14 @@
+-- 1.0 -> 1.1
+use Northwind;
+
+if object_id(N'EmployeeCreditCards', N'U') is null
+create table EmployeeCreditCards
+(
+	CreditCardID int identity (1, 1) not null,
+	CardNumber bigint not null,
+	ExpiredDate datetime not null,
+	CardHolder varchar not null,
+	EmployeeID int not null,
+	constraint PK_EmployeeCreditCards primary key (CreditCardID),
+	constraint FK_EmployeeCreditCards_Employees foreign key (EmployeeID) references Employees (EmployeeID)
+);
