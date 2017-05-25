6d12df413fea304876dbd13c14e4cbbc69663a0f
Natalia Vladimirova <Natalia_Vladimirova@epam.com>
2017-05-25 11:06:07 +0300

    Task 6 part 1 - add task 2.3.1

--- Task6.BasicSQL/Part01.SQL.Scripts/2.3.sql
+++ Task6.BasicSQL/Part01.SQL.Scripts/2.3.sql
@@ -1,4 +1,13 @@
 --2.3.1
+select distinct e.EmployeeID, e.FirstName + ' ' + e.LastName as FullName
+from Employees e
+join EmployeeTerritories et
+on e.EmployeeID = et.EmployeeID
+join Territories t
+on et.TerritoryID = t.TerritoryID
+join Region r
+on r.RegionID = t.RegionID
+where r.RegionDescription = 'Western'
 
 
 --2.3.2
