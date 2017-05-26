c18bb087f6da96e325ab3ef7427f124f963421a1
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2017-05-26 21:47:32 +0300

    Task 6 part 1 - fix 1.2.1 and 1.2.2

--- Task6.BasicSQL/Part01.SQL.Scripts/1.2.sql
+++ Task6.BasicSQL/Part01.SQL.Scripts/1.2.sql
@@ -1,12 +1,12 @@
 --1.2.1
-select CustomerID, Country from Customers
+select ContactName, Country from Customers
 where Country in ('USA', 'Canada')
-order by CustomerID, Country
+order by ContactName, Country
 
 --1.2.2
-select CustomerID, Country from Customers
+select ContactName, Country from Customers
 where Country not in ('USA', 'Canada')
-order by CustomerID
+order by ContactName
 
 --1.2.3
 select distinct Country from Customers
