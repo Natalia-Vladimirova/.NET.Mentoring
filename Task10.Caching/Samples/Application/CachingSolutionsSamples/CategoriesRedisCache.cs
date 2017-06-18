4a831bcd4c5bf918dcad4800f12d66b9a3ed5e53
Natalia Vladimirova <natalia.vladimirova.96@gmail.com>
2017-06-18 22:14:30 +0300

    Task 10 - add initial CachingSolutionsSamples

---
+++ Task10.Caching/Samples/Application/CachingSolutionsSamples/CategoriesRedisCache.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using NorthwindLibrary;
+using StackExchange.Redis;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace CachingSolutionsSamples
+{
+    public class CategoriesRedisCache : ICategoriesCache
+	{
+        private readonly string _prefix = "Cache_Categories";
+		private readonly ConnectionMultiplexer _redisConnection;
+        private readonly DataContractSerializer _serializer;
+
+		public CategoriesRedisCache(string hostName)
+		{
+			_redisConnection = ConnectionMultiplexer.Connect(hostName);
+            _serializer = new DataContractSerializer(typeof(IEnumerable<Category>));
+        }
+
+		public IEnumerable<Category> Get(string forUser)
+		{
+			var db = _redisConnection.GetDatabase();
+			byte[] value = db.StringGet(_prefix + forUser);
+
+			return value == null
+                ? null
+                : (IEnumerable<Category>)_serializer.ReadObject(new MemoryStream(value));
+		}
+
+		public void Set(string forUser, IEnumerable<Category> categories)
+		{
+			var db = _redisConnection.GetDatabase();
+			var key = _prefix + forUser;
+
+			if (categories == null)
+			{
+				db.StringSet(key, RedisValue.Null);
+			}
+			else
+			{
+				var stream = new MemoryStream();
+				_serializer.WriteObject(stream, categories);
+				db.StringSet(key, stream.ToArray());
+			}
+		}
+	}
+}
